4b0bd9560d3c42fb3df64906433accf30b52c12c
Enrico Schumann <es@enricoschumann.net>
2015-05-15 17:21:31 +0200

    * DESCRIPTION (Version): Version: 0.2-0

---
+++ R/readLogfile.R
@@ -0,0 +1,79 @@
+## file <- "/var/games/nethack/logfile"
+## readLogfile(file)
+
+readLogfile <- function(file, version = "3.4.3", expand.abbrevs = TRUE) {
+
+    field <- function(x, n)
+        unlist(lapply(x,`[[`, n))
+    
+    txt <- readLines(file)
+
+    ## version number present?
+    ver <- grep("^[0-9][.][0-9][.][0-9].*", txt)
+
+    ## (i) with version number (since 3.2.0)
+    comma <- regexpr(",", txt, fixed = TRUE)    
+    txt0 <- substr(txt[ver], 1, comma - 1L)
+    tmp <- strsplit(txt0, " ")
+
+    version       <- field(tmp, 1)
+    score         <- as.numeric(field(tmp, 2))
+    dun.num       <- as.numeric(field(tmp, 3))
+    dun.level     <- as.numeric(field(tmp, 4))
+    dun.level.max <- as.numeric(field(tmp, 5))
+    hp            <- as.numeric(field(tmp, 6))
+    hp.max        <- as.numeric(field(tmp, 7))
+    ndeaths       <- as.numeric(field(tmp, 8))
+    end.date      <- as.Date(field(tmp, 9), format = "%Y%m%d")
+    start.date    <- as.Date(field(tmp, 10), format = "%Y%m%d")
+    uid           <- as.numeric(field(tmp, 11))
+    role          <- field(tmp, 12)
+    race          <- field(tmp, 13)
+    gender        <- field(tmp, 14)
+    alignment     <- field(tmp, 15)
+    name          <- field(tmp, 16)
+    cause         <- substr(txt[ver], comma + 1L, nchar(txt))
+                                
+    ## TODO: roles
+    if (expand.abbrevs) {
+       role[role == "Bar"] <- "Barbarian"
+       role[role == "Hea"] <- "Healer"
+       role[role == "Mon"] <- "Monk"
+       role[role == "Ran"] <- "Ranger"
+       role[role == "Rog"] <- "Rogue"
+       role[role == "Sam"] <- "Samurai"
+       role[role == "Tou"] <- "Tourist"
+       role[role == "Wiz"] <- "Wizard"
+       ##
+       gender[gender == "Mal"] <- "male"
+       gender[gender == "Fem"] <- "female"
+       ##
+       alignment[alignment == "Neu"] <- "neutral"
+       alignment[alignment == "Cha"] <- "chaotic"
+       alignment[alignment == "Law"] <- "lawful"
+       ##
+       race[race == "Hum"] <- "human"
+       race[race == "Elf"] <- "elf"
+       race[race == "Orc"] <- "orc"
+    }
+
+    df0 <- data.frame(version       =  version, 
+                      score         =  score, 
+                      dun.num       =  dun.num, 
+                      dun.level     =  dun.level, 
+                      dun.level.max =  dun.level.max, 
+                      hp            =  hp, 
+                      hp.max        =  hp.max, 
+                      ndeaths       =  ndeaths, 
+                      end.date      =  end.date, 
+                      start.date    =  start.date, 
+                      uid           =  uid, 
+                      role          =  role, 
+                      race          =  race, 
+                      gender        =  gender, 
+                      alignment     =  alignment, 
+                      name          =  name, 
+                      cause         =  cause)
+
+    df0
+}
