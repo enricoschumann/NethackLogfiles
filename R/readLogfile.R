4b50f827876fe91ecddb5ad499a8a14fad79b596
Enrico Schumann <es@enricoschumann.net>
2018-01-02 17:46:08 +0100

    Support xlogfiles
    * DESCRIPTION (Version): 0.9-0
    
    * R/readLogfile.R (read_xlogfile): add function
    
    * R/readLogfile.R: rename function
    	   readLogfile => read_logfile

--- R/readLogfile.R
+++ R/readLogfile.R
@@ -1,87 +1,74 @@
-## file <- "/var/games/nethack/logfile"
-
-## file <- "~/Downloads/Nethack/games/lib/nethackdir/xlogfile"
-read_xlogfile <- function(file, version = "auto", expand.abbrevs = TRUE) {
+read_xlogfile <- function(file,
+                          expand.abbrevs = TRUE,
+                          sep = "\t") {
 
     txt <- readLines(file)
-    ver <- grep("version=", txt, perl = TRUE)
-    version_ <- if (length(ver))
-                    gsub("version=([0-9]+[.][0-9]+[.][0-9]+).*",
-                         "\\1", txt[min(ver)])
-                else
-                    NA
 
-    if (version == "auto") {
-        if (!length(ver))
-            stop("no version information in logfile")
-        if (grepl("[0-9]+[.][0-9]+[.][0-9]+", version_))
-            version <- version_
-        else
-            stop("no version information in logfile")            
-    } else if (!is.na(version_) && (version_ != version))
-        warning("logfile version is ", sQuote(version_))
-
-
-    field <- function(s, field) {
-        ans <- lapply(tmp, strsplit, "=")
-        unlist(ans, `[[`, 1L)
+    field <- function(s, field, sep)
+        gsub(paste0(".*", field, "=([^", sep, "]+).*"), "\\1", s)
+    
+    version       <- field(txt, "version", sep)
+    score         <- as.numeric(field(txt, "points", sep))
+    dun.num       <- as.numeric(field(txt, "deathdnum", sep))
+    dun.level     <- as.numeric(field(txt, "deathlev", sep))
+    dun.level.max <- as.numeric(field(txt, "maxlvl", sep))
+    hp            <- as.numeric(field(txt, "hp", sep))
+    hp.max        <- as.numeric(field(txt, "maxhp", sep))
+    ndeaths       <- as.numeric(field(txt, "deaths", sep))
+    end.date      <- as.Date(field(txt, "deathdate", sep), format = "%Y%m%d")
+    start.date    <- as.Date(field(txt, "birthdate", sep), format = "%Y%m%d")
+    uid           <- as.numeric(field(txt, "uid", sep))
+    role          <- field(txt, "role", sep)
+    race          <- field(txt, "race", sep)
+    gender        <- field(txt, "gender", sep)
+    alignment     <- field(txt, "align", sep)
+    name          <- field(txt, "name", sep)
+    cause         <- field(txt, "death", sep)
+    while_ <- character(length(txt))
+    w_ <- grepl("while=", txt)
+    while_[w_] <- field(txt[w_], "while", sep)
+    conduct <- field(txt, "conduct", sep)
+    turns <- field(txt, "turns", sep)
+    achieve <- field(txt, "achieve", sep)
+    realtime <- field(txt, "realtime", sep)
+    starttime <- field(txt, "starttime", sep)
+    endtime <- field(txt, "endtime", sep)
+    gender0 <- field(txt, "gender0", sep)
+    align0 <- field(txt, "align0", sep)
+    flags <- field(txt, "flags", sep)
+    
+    if (expand.abbrevs) {
+        
+        role[role == "Arc"] <- "archeologist"
+        role[role == "Bar"] <- "barbarian"
+        role[role == "Cav"] <- "caveman"
+        role[role == "Hea"] <- "healer"
+        role[role == "Kni"] <- "knight"
+        role[role == "Mon"] <- "monk"
+        role[role == "Pri"] <- "priest"
+        role[role == "Ran"] <- "ranger"
+        role[role == "Rog"] <- "rogue"
+        role[role == "Sam"] <- "samurai"
+        role[role == "Tou"] <- "tourist"
+        role[role == "Val"] <- "valkyrie"
+        role[role == "Wiz"] <- "wizard"
+        ##
+        gender[gender == "Mal"] <- "male"
+        gender[gender == "Fem"] <- "female"
+        gender0[gender0 == "Mal"] <- "male"
+        gender0[gender0 == "Fem"] <- "female"
+        ##
+        alignment[alignment == "Neu"] <- "neutral"
+        alignment[alignment == "Cha"] <- "chaotic"
+        alignment[alignment == "Law"] <- "lawful"
+        ##
+        race[race == "Dwa"] <- "dwarf"
+        race[race == "Elf"] <- "elf"
+        race[race == "Gno"] <- "gnome"
+        race[race == "Hum"] <- "human"
+        race[race == "Orc"] <- "orc"
     }
     
-    if (version %in% c("3.4.3", "3.6.0")) {
-        tmp <- strsplit(txt, "\t")
-        
-        version       <- field(tmp, 1)
-        score         <- as.numeric(field(tmp, 2))
-        dun.num       <- as.numeric(field(tmp, 3))
-        dun.level     <- as.numeric(field(tmp, 4))
-        dun.level.max <- as.numeric(field(tmp, 5))
-        hp            <- as.numeric(field(tmp, 6))
-        hp.max        <- as.numeric(field(tmp, 7))
-        ndeaths       <- as.numeric(field(tmp, 8))
-        end.date      <- as.Date(field(tmp, 9), format = "%Y%m%d")
-        start.date    <- as.Date(field(tmp, 10), format = "%Y%m%d")
-        uid           <- as.numeric(field(tmp, 11))
-        role          <- field(tmp, 12)
-        race          <- field(tmp, 13)
-        gender        <- field(tmp, 14)
-        alignment     <- field(tmp, 15)
-        name          <- field(tmp, 16)
-        cause         <- substr(txt[ver], comma + 1L, nchar(txt))
-        
-        
-        if (expand.abbrevs) {
-            
-            role[role == "Arc"] <- "archeologist"
-            role[role == "Bar"] <- "barbarian"
-            role[role == "Cav"] <- "caveman"
-            role[role == "Hea"] <- "healer"
-            role[role == "Kni"] <- "knight"
-            role[role == "Mon"] <- "monk"
-            role[role == "Pri"] <- "priest"
-            role[role == "Ran"] <- "ranger"
-            role[role == "Rog"] <- "rogue"
-            role[role == "Sam"] <- "samurai"
-            role[role == "Tou"] <- "tourist"
-            role[role == "Val"] <- "valkyrie"
-            role[role == "Wiz"] <- "wizard"
-            ##
-            gender[gender == "Mal"] <- "male"
-            gender[gender == "Fem"] <- "female"
-            ##
-            alignment[alignment == "Neu"] <- "neutral"
-            alignment[alignment == "Cha"] <- "chaotic"
-            alignment[alignment == "Law"] <- "lawful"
-            ##
-            race[race == "Dwa"] <- "dwarf"
-            race[race == "Elf"] <- "elf"
-            race[race == "Gno"] <- "gnome"
-            race[race == "Hum"] <- "human"
-            race[race == "Orc"] <- "orc"
-        }
-        
-    } else
-        stop("not sure how to handle this version")
-
     data.frame(version       =  version, 
                name          =  name, 
                score         =  score, 
@@ -99,6 +86,7 @@ read_xlogfile <- function(file, version = "auto", expand.abbrevs = TRUE) {
                gender        =  gender, 
                alignment     =  alignment, 
                cause         =  cause,
+               `while`       =  while_, 
                stringsAsFactors = FALSE)
     
 }
@@ -225,6 +213,7 @@ read_logfile <- function(file, version = "auto", expand.abbrevs = TRUE) {
                       gender        =  gender, 
                       alignment     =  alignment, 
                       cause         =  cause,
+                      `while`       =  NA,
                       stringsAsFactors = FALSE)
 
     df0
