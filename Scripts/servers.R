9aee03eb33f5b2359d4cd50361c78c9b336c69ad
Enrico Schumann <es@enricoschumann.net>
2025-01-07 09:24:39 +0100

    Remove examples

--- Scripts/servers.R
+++ Scripts/servers.R
@@ -1,16 +1,9 @@
 library("NethackLogfiles")
 
-txt <- readLines("https://eu.hardfought.org/xlogfiles/nethack36/xlogfile")
-
-log <- read_xlogfile("https://eu.hardfought.org/xlogfiles/nethack36/xlogfile",
-                     iconv.sub = "_")
-
+file <- "https://eu.hardfought.org/xlogfiles/nethack36/xlogfile"
+txt <- readLines(file)
+log <- read_xlogfile(file, iconv.sub = "_")
 
 
-
-xx <- iconv(txt[[967]], from = "cp1252", sub = "_")
-strsplit(xx, "\t")
-
-## x <- "fa\xE7ile"
-xx <- iconv(lf, "latin1", "UTF-8")
-xx <- iconv(xx, "UTF-8", "ASCII", "c99")
+## xx <- iconv(txt[[967]], from = "cp1252", sub = "_")
+## strsplit(xx, "\t")
